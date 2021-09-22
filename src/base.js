0f0fd3f6f2ef89f1b15d5ce0bdf97e555813f578
Isuru-Maldeniya <isurumaldeniya@gmail.com>
2021-09-22 16:56:53 +0530

    done

--- src/base.js
+++ src/base.js
@@ -1,2 +1,2 @@
 // export const Base = 'http://localhost:8080';
-export const Base = 'https://deployment-2-drivelearnbackend.herokuapp.com';
+export const Base = ' https://drivelrn-backend.herokuapp.com';
